d51d498a23e9ce9eaaeb2aa34ce1ea2c464c05e3
Ptashynskyi <n.ptashynskyi@gmail.com>
2019-06-11 17:16:25 +0300

    Add refactoring and income method

--- script.js
+++ script.js
@@ -15,28 +15,64 @@ let appData = {
     optionalExpenses: {},
     income: [],
     timeData: time,
-    savings: true
+    savings: true,
+    chooseExpenses: function() {
+        for (let i = 0; i < 1; i++) {
+            let a = prompt("Enter a mandatory expense article this month ", ""),
+                b = prompt("How much will it cost ? ", "");
+            if (
+                typeof a === "string" &&
+                typeof a != null &&
+                typeof b != null &&
+                a != "" &&
+                b != "" &&
+                a.length < 50
+            ) {
+                appData.expenses[a] = b;
+            } else {
+                i = i - 1;
+            }
+        }
+    },
+    detectDayBudget: function() {
+        appData.moneyPerDay = (appData.budget / 30).toFixed(2);
+        alert("Budget for 1 day is " + appData.moneyPerDay + "uan");
+    },
+    detectedLevel: function() {
+        if (appData.moneyPerDay < 100) {
+            console.log("This is the minimum income level!");
+        } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+            console.log("This is the average income level!");
+        } else if (appData.moneyPerDay > 2000) {
+            console.log("This is a high income!");
+        } else {
+            console.log("Error");
+        }
+    },
+    checkSavings: function() {
+        if (appData.savings == true) {
+            let save = +prompt("What amount of money on the deposit?"),
+                percent = +prompt("what percentage ?");
+
+            appData.monthIncome = (save / 100 / 12) * percent;
+            alert("your monthly deposit income is" + appData.monthIncome);
+        }
+    },
+    chooseOptExpenses: function() {
+        for (let i = 0; i < 3; i++) {
+            let opt = prompt("an item of non-compulsory expenditure", "");
+            appData.optionalExpenses[i] = opt;
+        }
+    },
+    chooseIncome: function() {
+        let items = prompt("enter the gender of additional income", "");
+        appData.income = split.items(", ");
+        appData.income.push(prompt("Is it all?", ""));
+        appData.income.sort();
+    }
 };
 
-function chooseExpenses() {
-    for (let i = 0; i < 1; i++) {
-        let a = prompt("Enter a mandatory expense article this month ", ""),
-            b = prompt("How much will it cost ? ", "");
-        if (
-            typeof a === "string" &&
-            typeof a != null &&
-            typeof b != null &&
-            a != "" &&
-            b != "" &&
-            a.length < 50
-        ) {
-            appData.expenses[a] = b;
-        } else {
-            i = i - 1;
-        }
-    }
-}
-chooseExpenses();
+
 /*let i = 0;
 while(i<2){
 	 let a = prompt("Enter a mandatory expense article this month", ''),
@@ -76,14 +112,3 @@ if (appData.moneyPerDay < 100) {
 } else {
     console.log("Error");
 }
-
-function checkSavings() {
-    if (appData.savings == true) {
-        let save = +prompt("What amount of money on the deposit?"),
-            percent = +prompt("what percentage ?");
-
-        appData.monthIncome = (save / 100 / 12) * percent;
-        alert("your monthly deposit income is" + appData.monthIncome);
-    }
-}
-checkSavings();
